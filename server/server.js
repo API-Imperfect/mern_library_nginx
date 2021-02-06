0ea389b6a46c4f13ce40e7d437cc2a04d7f34510
Ogilo <alphaogilo@gmail.com>
2021-02-06 15:13:29 +0200

    docs: add comments to relevant files

--- server/server.js
+++ server/server.js
@@ -2,20 +2,25 @@ require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/db");
 
+//Initialize the app
 const app = express();
 
 connectToDB();
 
+//enable our app to parse JSON
 app.use(express.json());
 
+//declare our PORT
 const PORT = process.env.PORT || 5000;
 
+//Mount/Create Routes
 app.get("/test", (req, res) => {
     res.json({
         Hi: "Welcome to the MERN Library API",
     });
 });
 
+// Make the sever listen on the declared PORT variable
 app.listen(
     PORT,
     console.log(
