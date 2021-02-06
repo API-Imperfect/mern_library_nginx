52463a51c746c3a14e3ca3330d9d40448ccb2ef5
Ogilo <alphaogilo@gmail.com>
2021-02-06 12:19:15 +0200

    chore: setup basic express server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/test", (req, res) => {
+    res.json({
+        Hi: "Welcome to the MERN Library API",
+    });
+});
+
+app.listen(
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+);
